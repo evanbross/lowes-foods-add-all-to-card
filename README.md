c583d315fcfc4360e1672cb9ee8ebfe480249435
Evan Ross <evan@eross.com>
2024-11-22 07:01:45 -0500

    sdf

---
+++ README.md
@@ -0,0 +1,17 @@
+# Food Lion - Load All To Card
+
+## Introduction
+
+This is a Chrome extension that adds a "Load All To Card" button to the Food Lion [MVP Coupons Center](https://foodlion.com/savings/coupons/browse). The Javascript action on this button will click on all the coupons in one shot, adding all of them to the card.
+
+## Installation
+
+git clone this repository to your local disk or download the zip file and extract.
+
+Go to the Chrome extensions page (chrome://extensions). You'll need to enable developer mode in order to load an unpacked extension.
+
+Click on "Load unpacked extension" and navigate to the folder in which you cloned or extracted the source files. Select this folder to load the extension.
+
+## Usage
+
+Once you've installed and enabled this extension, go to the Food Lion MVP Coupons Center and find the "Load All To Card" button on the top left corner. Click on it and it will load all the coupons to your card.
---
+++ main.js
@@ -0,0 +1,117 @@
+// jshint esversion: 8
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function findButtons(str) {
+  let btns = [];
+  for (let btn of document.querySelectorAll('button')) {
+    if (btn.textContent.includes(str)) {
+      btns.push(btn);
+    }
+  }
+  return btns;
+}
+
+async function runSelect(event) {
+  event.preventDefault();
+
+  // Click on "Show More" button until it no longer shows up.
+  // Limit the number of times we click on that button.
+  for (let i = 0; i < 10; ++i) {
+    let btns = findButtons('Show More');
+    if (!btns.length) break;
+    let btn = btns[0];
+    btn.scrollIntoView();
+    btn.click();
+    await sleep(1000);
+  }
+
+  // Click on "Clip Coupon" buttons.
+  let btns = findButtons('Clip!');
+  console.log(btns.length + ' coupons found');
+
+  let clicked = 0;
+  for (let btn of Array.from(btns).reverse()) {
+    btn.scrollIntoView();
+    btn.click();
+    clicked++;
+    await sleep(1000);
+  }
+
+  console.log(clicked + ' coupons clicked');
+}
+
+function insertButton(btn) {
+  function waitForSite() {
+    let targetelem = document.getElementById('js-app-content');
+	console.log('Button will go before '+targetelem);
+    if (targetelem !== null) {
+      clearInterval(waitForSiteTimer);
+      targetelem.insertBefore(btn, targetelem.childNodes[0]);
+    }
+  }
+
+	console.log('insertButton() begin');
+  // Wait for site to finish loading before inserting button.
+  let waitForSiteTimer = setInterval(waitForSite, 100);
+}
+
+function run() {
+  console.log('run() begin');
+  // Check if we are on the coupons page.
+  let loc = window.location.href;
+  if (!loc.includes('/promotions')) {
+    // Remove button if it is on a non-coupon page.
+    let btn = document.getElementById('load_all_to_card');
+    if (btn) btn.remove();
+    return;
+  }
+
+  // Make a new button for our action.
+  let newbutton = document.createElement('button');
+  newbutton.name = 'load_all_to_card';
+  newbutton.id = 'load_all_to_card';
+  newbutton.style.cssText = 'background-color: #fff; color: #E82A24; font-weight: 700; border: solid #E82A24; padding: 6px 10px; cursor: pointer; margin: 5px';
+  console.log('new button created');
+
+  newbutton.addEventListener('mouseenter',
+    () => {
+      newbutton.style.color = '#fff';
+      newbutton.style.backgroundColor = '#E82A24';
+    }
+  );
+
+  newbutton.addEventListener('mouseleave',
+    () => {
+      newbutton.style.color = '#E82A24';
+      newbutton.style.backgroundColor = '#fff';
+    }
+  );
+
+  newbutton.appendChild(document.createTextNode('Load All To Card'));
+  newbutton.addEventListener('click', runSelect);
+
+  // Don't add button if already there.
+  if (!document.getElementById('load_all_to_card')) insertButton(newbutton);
+}
+
+// Run the button inserter the first time and also whenever the URL changes.
+// Some links in the new Food Lion web interface do not reload the page.
+const observeUrlChange = () => {
+	console.log('observeUrlChange begin');
+  let oldHref = null;
+  const body = document.querySelector('body');
+  const observer = new MutationObserver(mutations => {
+    if (oldHref !== document.location.href) {
+      oldHref = document.location.href;
+      run();
+    }
+  });
+  observer.observe(body, { childList: true, subtree: true });
+};
+
+window.onload = observeUrlChange;
+
+// -- The End --
